b077f5a97208f08ce3ca18d1a8685493a6660881
Boluwatife Olabiran <boluolabiran@gmail.com>
2024-02-29 10:23:13 -0500

    Made launching joystick driver optional and fixed compilation issues due to parameters without defaults.

--- raptor_dbw_joystick/src/raptor_dbw_joystick_node.cpp
+++ raptor_dbw_joystick/src/raptor_dbw_joystick_node.cpp
@@ -39,10 +39,10 @@ int main(int argc, char ** argv)
 
   // Get parameter values
   auto temp = std::make_shared<rclcpp::Node>("get_joy_params_node", options);
-  temp->declare_parameter("ignore", "False");
-  temp->declare_parameter("enable", "True");
-  temp->declare_parameter("svel", 200.0);
-  temp->declare_parameter("max_steer_angle", 470.0);
+  temp->declare_parameter("ignore", rclcpp::PARAMETER_BOOL);
+  temp->declare_parameter("enable", rclcpp::PARAMETER_BOOL);
+  temp->declare_parameter("svel", rclcpp::PARAMETER_DOUBLE);
+  temp->declare_parameter("max_steer_angle", rclcpp::PARAMETER_DOUBLE);
 
   bool n_ignore = temp->get_parameter("ignore").as_bool();
   bool n_enable = temp->get_parameter("enable").as_bool();
